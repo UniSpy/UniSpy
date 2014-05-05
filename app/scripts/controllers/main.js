bbf411a65884ed91293713739b6131087041cc1b
Kaikkonen Antti J I <anttkaik@da1-cs-b221-25.helsinki.fi>
2014-05-05 16:45:28 +0300

    filterointia yms

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,25 @@
 'use strict';
 
 angular.module('uniSpyApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+
+    
+
+    $http.get('menus.json').success( function(data, status, headers, config) {
+        console.log(data)
+        $scope.entries = data;
+    });
+
+    $scope.onlyUnicafe = function(entry) {
+      console.log(this, arguments)
+      console.log("auts")
+      return entry.campus === "Kumpula";
+    }
+
   });
