fb3a4502c532c900d8d42e7f12afa013bcb13824
weequ <antti.kaik@gmail.com>
2014-05-08 13:18:57 +0300

    login cotrolleria

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,10 +1,55 @@
 'use strict';
 
+var app = angular.module('uniSpyApp');
+
+
+app.factory('Auth', function($http){
+    var URL = 'http://unispy-backend.herokuapp.com/session';
+    var service = {};
+
+    service.logged = {};
+
+    service.login = function(credentials) {
+      console.log("Logging in :D")
+      return $http.post(URL, credentials).then(
+        function (token) {
+          service.logged.status = true;
+          service.logged.user = credentials.user;
+          $http.defaults.headers.common['auth-token'] = token.data;
+          return token.data;
+        }
+      )
+    }
+
+    service.logout = function(credentials) {
+      return $http.delete(URL).then(
+        function () {
+          service.logged.status = false;
+          service.logged.user = null;
+          delete $http.defaults.headers.common['auth-token']
+          return null;
+        }
+      )
+    }
+
+    return service;
+});
+
+
 angular.module('uniSpyApp')
-  .controller('LoginCtrl', function ($scope) {
+  .controller('LoginCtrl', function ($scope, Auth) {
   	  $scope.isCollapsed = true;
 
   	  $scope.login = function (){
   	  	console.log("BOOO!½");
+  	  	Auth.login($scope.credentials)
+	      .then(
+	        function(data) {
+	          // success
+	        },function( data ) {
+	          // failure
+	        }
+	      );
+	      $scope.credentials = {}
   	  }
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+
 angular.module('uniSpyApp')
   .controller('MainCtrl', function ($scope, $http, $filter) {
 
