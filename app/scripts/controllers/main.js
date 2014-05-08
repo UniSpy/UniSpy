8ee9ddeb6056a196d3f2e3ddce83dcb23f45b163
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-05-08 17:04:43 +0300

    deleting unnecessary files and translating some

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,9 +10,6 @@ angular.module('uniSpyApp')
 
     $http.get('http://unispy-backend.herokuapp.com/menus.json').success( function(data, status, headers, config) {
       $scope.entries = data;
-      if ($scope.tags !== undefined) {
-        makeRecommedations();
-      }
     });
 
     $http.get('http://unispy-backend.herokuapp.com/tags.json').success( function(data, status, headers, config) {
@@ -20,47 +17,7 @@ angular.module('uniSpyApp')
       for (var i in data) {
         $scope.tags.push(data[i].content);
       }
-      //$scope.tags = data;
-      console.log($scope.tags);
-      if ($scope.entries !== undefined) {
-        makeRecommedations();
-      }
     });
-
-    function makeRecommedations() {
-      //$scope.recommendations = $filter($scope.entries, )
-      $scope.recommendations = angular.copy($scope.entries);
-      for (var place in $scope.entries) {
-        var currentPlace = $scope.entries[place];
-        if (currentPlace.meals === undefined) {
-          continue;
-        }
-        var allDaysDeleted = true;
-        for (var day in currentPlace.meals.fi) {
-          var currentDay = currentPlace.meals.fi[day];
-          var allMealsdeleted = true;
-          var meals = [];
-          for (var meal in currentDay) {
-            var currentMeal = currentDay[meal];
-            if ($scope.isTagged(currentMeal)) {
-              meals.push(currentMeal);
-              allMealsdeleted = false;
-            } else {
-            }
-          }
-          //$scope.recommendations[place].meals.fi[day] = meals;
-          $scope.recommendations[place].meals.fi[day] = meals;
-          if (allMealsdeleted) {
-          } else {
-            allDaysDeleted = false;
-          }
-        }
-        if (allDaysDeleted) {
-          delete $scope.recommendations[place];
-        }
-      }
-      console.log($scope.recommendations);
-    }
 
     $scope.taggedOnly = false;
 
@@ -81,7 +38,7 @@ angular.module('uniSpyApp')
       return entry.name.indexOf("Unicafe") > -1;
     };
 
-    $scope.onlyUnicafeWithFinnnishMeals = function(entry) {
+    $scope.onlyUnicafeWithFinnishMeals = function(entry) {
       return $scope.onlyUnicafe(entry) && entry.meals.fi.length > 0;
     }
 
